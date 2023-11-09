f0d10e0be7347e347aeca34a7878ace9d9d626ac
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-11-09 10:24:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let a=prompt("Enter your age");
+if(a<18){
+    console.log('chala ja')
+}
+if(a>18){
+    if(a>25){
+        console.log('le maza le')
+    }
+    if(a<25 && a>18){
+        console.log('ishq di gali wich no entry');
+        
+    }
+} 
